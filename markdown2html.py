9f5d714d56f36e1303146dd8a8afef3e38d6c55b
Zakaria Elkhadir <121963218+ZakariaElkhadir@users.noreply.github.com>
2024-08-19 20:24:37 +0100

    add file

---
+++ markdown2html.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+import os
+
+
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    exit(1)
+
+markdown_file = sys.argv[1]
+output = sys.argv[2]
+
+if not os.path.isfile(markdown_file):
+    print("Missing <filename>", file=sys.stderr)
+    exit(1)
+
+with open(markdown_file, "r") as file:
+    content = file.readlines()
+
+
+content_mark = "good"
+header = {
+    "#": lambda: f"<h1>{content_mark}</h1>",
+    "##": lambda: f"<h2>{content_mark}</h2>",
+    "###": lambda: f"<h3>{content_mark}</h3>",
+    "####": lambda: f"<h4>{content_mark}</h4>",
+    "#####": lambda: f"<h5>{content_mark}</h5>",
+    "######": lambda: f"<h6>{content_mark}</h6>",
+}
+for i in content:
+    # prob here
+    if i in header:
+        print(header[i]())
+
+exit(0)
