7e5685e5738c02fa3fa091138e041c7508297e7e
Zakaria Elkhadir <121963218+ZakariaElkhadir@users.noreply.github.com>
2024-08-25 12:32:36 +0100

    add file

--- markdown2html.py
+++ markdown2html.py
@@ -3,16 +3,15 @@ import sys
 import os
 
 if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    exit(1)
 
-input_file = sys.argv[1]
+markdown_file = sys.argv[1]
 output_file = sys.argv[2]
 
-if not os.path.isfile(input_file):
-    sys.stderr.write("Missing " + input_file + "\n")
-    sys.exit(1)
+if not os.path.isfile(markdown_file):
+    print(f"Missing {markdown_file}", file=sys.stderr)
+    exit(1)
 
-# Convert Markdown to HTML and save to output_file
 
-sys.exit(0)
+exit(0)
