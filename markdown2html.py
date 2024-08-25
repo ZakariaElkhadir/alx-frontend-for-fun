cc17ee55e83338824f92fa22e91833d167efcbb7
Zakaria Elkhadir <121963218+ZakariaElkhadir@users.noreply.github.com>
2024-08-25 12:22:40 +0100

    refactor: Improve markdown to HTML conversion in markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""doc doc"""
+"""Markdown to HTML converter"""
 import sys
 import os
-
 
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
@@ -12,26 +11,23 @@ markdown_file = sys.argv[1]
 output = sys.argv[2]
 
 if not os.path.isfile(markdown_file):
-    print("Missing <filename>", file=sys.stderr)
+    print(f"Missing {markdown_file}", file=sys.stderr)
     exit(1)
 
 with open(markdown_file, "r") as file:
     content = file.readlines()
 
-
 def parse_line(line):
-    """doc doc"""
+    """Parses a line of Markdown and converts to HTML."""
     line = line.strip()
     if line.startswith('#'):
         heading_level = len(line) - len(line.lstrip('#'))
         if 1 <= heading_level <= 6:
             heading_content = line[heading_level:].strip()
             return f"<h{heading_level}>{heading_content}</h{heading_level}>"
-    return line
+    return None  # Ignore non-heading lines
 
-
-html_lines = [parse_line(line) for line in content]
-
+html_lines = [parse_line(line) for line in content if parse_line(line)]
 
 with open(output, "w") as html_file:
     html_file.write("\n".join(html_lines))
