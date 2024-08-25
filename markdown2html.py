acce3a229fe393bfe567aef09735090cbbe13d5e
Zakaria Elkhadir <121963218+ZakariaElkhadir@users.noreply.github.com>
2024-08-25 12:30:35 +0100

    add file

--- markdown2html.py
+++ markdown2html.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 import sys
 import os
 
 if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
     sys.exit(1)
 
 input_file = sys.argv[1]
